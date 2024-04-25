e25eee774eca8aee3295cdf5dcd6dd1e68e5c5f3
Omar Ahmed <omarahmed19991@outlook.com>
2024-04-25 13:06:23 +0200

    Add user orders page

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -66,4 +66,14 @@ const verifyOrder = async (req, res) => {
         res.json({success: false, message: "Error"})
     }
 }
-export { placeOrder, verifyOrder };
+//user orders for frontend
+const userOrder = async (req, res) => {
+    try {
+        const orders = await orderModel.find({userId: req.body.userId})
+        res.json({success: true, data: orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Error"})
+    }
+}
+export { placeOrder, verifyOrder, userOrder };
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import authMiddleWare from '../middleware/auth.js'
-import { placeOrder, verifyOrder } from '../controllers/orderController.js'
+import { placeOrder, userOrder, verifyOrder } from '../controllers/orderController.js'
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleWare, placeOrder);
 orderRouter.post("/verify", verifyOrder)
+orderRouter.post("/user-order", authMiddleWare, userOrder);
 
 export default orderRouter;
