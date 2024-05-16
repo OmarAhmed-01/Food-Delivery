707238b232a3b001ff4e5aca4f4b2dfd6f6452d7
Omar Ahmed <omarahmed19991@outlook.com>
2024-05-16 14:01:31 +0300

    updated URL

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,13 +1,16 @@
 import orderModel from '../models/orderModel.js'
 import userModel from '../models/userModels.js'
 import Stripe from 'stripe';
+import React, { useContext } from 'react';
+import StoreContext from '../../frontend/src/context/StoreContext.jsx'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placing order from frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = 'http://localhost:5173'
+    const { url } = useContext(StoreContext);
+
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
@@ -41,8 +44,8 @@ const placeOrder = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: 'payment',
-            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+            success_url: `${url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${url}/verify?success=false&orderId=${newOrder._id}`,
         })
         res.json({success: true, session_url: session.url})
     } catch (error) {
