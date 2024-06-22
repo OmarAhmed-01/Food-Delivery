76b2515b5fa11593ad13e8d1d7e9db4d1ce30044
Omar Ahmed <omarahmed19991@outlook.com>
2024-06-22 15:51:14 +0300

    Update url to fit deployment

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://deliveroo-backend-upf5.onrender.com";
 
   return (
     <div>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItem, setCartItems] = useState({});
-  const url = 'http://localhost:4000';
+  const url = 'https://deliveroo-backend-upf5.onrender.com';
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
